cc798570f87beac6501d62d896c9bbe0fdd5c61a
kasiwa-mesi <italynataly@outlook.jp>
2022-06-10 00:32:09 +0900

    :sparkles: create success dialog after save record

---
+++ lib/app/home/funky_overlay.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class FunkyOverlay extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => FunkyOverlayState();
+}
+
+class FunkyOverlayState extends State<FunkyOverlay>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 450));
+    scaleAnimation =
+        CurvedAnimation(parent: controller, curve: Curves.elasticInOut);
+
+    controller.addListener(() {
+      setState(() {});
+    });
+
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Material(
+        color: Colors.transparent,
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          child: Container(
+            height: 180,
+            decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  // side: BorderSide(
+                  //   color: Colors.grey.shade800,
+                  // ),
+                )),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 24,
+                left: 32,
+                right: 32,
+                bottom: 24,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.check_circle_rounded,
+                    color: Colors.green,
+                    size: 64,
+                  ),
+                  SizedBox(
+                    width: 12,
+                    height: 12,
+                  ),
+                  Text(
+                    "データを記録しました！",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
